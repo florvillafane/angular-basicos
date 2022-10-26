9607845985aba273df31d3cf2fdb6199ecee1d7b
florvillafane <florencia.a.villafane@gmail.com>
2022-10-26 16:34:44 -0300

    se cambian estilos y fuente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorModule } from './contador/contador.module';
 import { HeroesModule } from './heroes/heroes.module';
-
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
@@ -14,7 +14,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
